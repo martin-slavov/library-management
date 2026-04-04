2a88e218eb291d71bbbabb89b449cc04056d67cf
martin-slavov <mstefslav@gmail.com>
2026-04-05 01:13:44 +0300

    Add: DatabaseConnection class

--- src/main/java/com/github/martinslavov/Main.java
+++ src/main/java/com/github/martinslavov/Main.java
@@ -1,4 +1,19 @@
 package com.github.martinslavov;
 
+import com.github.martinslavov.config.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try (Connection conn = DatabaseConnection.getInstance().getConnection()) {
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
