55b1c2f16e94dfc8326797d737f31c5a1644e8a8
martin-slavov <mstefslav@gmail.com>
2026-04-05 20:01:02 +0300

    Add: BookDAO class

---
+++ src/main/java/com/github/martinslavov/dao/BookDAO.java
@@ -0,0 +1,175 @@
+package com.github.martinslavov.dao;
+
+import com.github.martinslavov.config.DatabaseConnection;
+import com.github.martinslavov.model.Book;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BookDAO {
+
+    public Optional<Book> save(Book book) {
+
+        String sql = "INSERT INTO books (ISBN, title, author_id, category_id, publication_date, total_copies, available_copies) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psInsertBook = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            psInsertBook.setString(1, book.getIsbn());
+            psInsertBook.setString(2, book.getTitle());
+            psInsertBook.setInt(3, book.getAuthorId());
+            psInsertBook.setInt(4, book.getCategoryId());
+            psInsertBook.setInt(5, book.getPublicationDate());
+            psInsertBook.setInt(6, book.getTotalCopies());
+            psInsertBook.setInt(7, book.getAvailableCopies());
+
+            psInsertBook.executeUpdate();
+
+            ResultSet generatedKeys = psInsertBook.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                book.setBookId(generatedKeys.getInt(1));
+            }
+
+            return Optional.of(book);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<Book> findById(int id) {
+
+        String sql = "SELECT * FROM books WHERE book_id = ?";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psFindById = conn.prepareStatement(sql)
+        ) {
+            psFindById.setInt(1, id);
+
+            ResultSet rs = psFindById.executeQuery();
+            if (rs.next()) {
+                return Optional.of(mapResultSetToBook(rs));
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Book> findAll() {
+
+        String sql = "SELECT * FROM books";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psFindAll = conn.prepareStatement(sql)
+        ) {
+            ResultSet rs = psFindAll.executeQuery();
+            List<Book> allBooks = new ArrayList<>();
+            while (rs.next()) {
+                allBooks.add(mapResultSetToBook(rs));
+            }
+            return allBooks;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Book> findByTitle(String title) {
+
+        String sql = "SELECT * FROM books WHERE title LIKE ?";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psFindByTitle = conn.prepareStatement(sql)
+        ) {
+            psFindByTitle.setString(1, "%" + title + "%");
+            ResultSet rs = psFindByTitle.executeQuery();
+            List<Book> bookByTitle = new ArrayList<>();
+            while (rs.next()) {
+                bookByTitle.add(mapResultSetToBook(rs));
+            }
+            return bookByTitle;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Book> findByAuthorId(int authorId) {
+
+        String sql = "SELECT * FROM books WHERE author_id = ?";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psFindByAuthorId = conn.prepareStatement(sql)
+        ) {
+            psFindByAuthorId.setInt(1, authorId);
+            ResultSet rs = psFindByAuthorId.executeQuery();
+            List<Book> bookByAuthorId = new ArrayList<>();
+            while (rs.next()) {
+                bookByAuthorId.add(mapResultSetToBook(rs));
+            }
+            return bookByAuthorId;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean update(Book book) {
+
+        String sql = """
+                UPDATE books SET ISBN = ?, title = ?, author_id = ?, category_id = ?,\s
+                publication_date = ?, total_copies = ?, available_copies = ?\s
+                WHERE book_id = ?""";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psUpdateBook = conn.prepareStatement(sql)
+        ) {
+            psUpdateBook.setString(1, book.getIsbn());
+            psUpdateBook.setString(2, book.getTitle());
+            psUpdateBook.setInt(3, book.getAuthorId());
+            psUpdateBook.setInt(4, book.getCategoryId());
+            psUpdateBook.setInt(5, book.getPublicationDate());
+            psUpdateBook.setInt(6, book.getTotalCopies());
+            psUpdateBook.setInt(7, book.getAvailableCopies());
+            psUpdateBook.setInt(8, book.getBookId());
+
+            return psUpdateBook.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean delete(int id) {
+
+        String sql = "DELETE FROM books WHERE book_id = ?";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psDeleteBook = conn.prepareStatement(sql)
+        ) {
+            psDeleteBook.setInt(1, id);
+            return psDeleteBook.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean updateAvailableCopies(int bookId, int delta) {
+
+        String sql = "UPDATE books SET available_copies = available_copies + ? WHERE book_id = ?";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement psUpdateBook = conn.prepareStatement(sql)
+        ) {
+            psUpdateBook.setInt(1, delta);
+            psUpdateBook.setInt(2, bookId);
+
+            return psUpdateBook.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Book mapResultSetToBook(ResultSet rs) throws SQLException {
+        return new Book(
+                rs.getInt("book_id"),
+                rs.getString("ISBN"),
+                rs.getString("title"),
+                rs.getInt("author_id"),
+                rs.getInt("category_id"),
+                rs.getInt("publication_date"),
+                rs.getInt("total_copies"),
+                rs.getInt("available_copies"));
+    }
+}
